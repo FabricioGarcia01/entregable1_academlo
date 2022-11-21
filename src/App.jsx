f1a2c98639e34adbfceb70c3f5a114bfe3f13790
FabricioGarcia01 <lasften999@hotmail.com>
2022-11-21 17:37:03 -0500

    2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
 import QuoteBox from "./components/QuoteBox"
 import quoteDB from "./db/quotes.json"
-import colors from "./db/colors"
-import {useState} from "react"
-
-
+import colors from "./db/colors.js"
+import { useState } from "react"
 
 function App() {
-
-  const getRandom=(array)=>{
-    const randomIndex=Math.floor(array.length*Math.random())
+  const getRandom = (array) => {
+    const randomIndex = Math.floor(array.length*Math.random())
     return array[randomIndex]
   }
-  const[quote,setQuote]=useState(getRandom(quoteDB)) 
-  const[color,setColor]=useState(getRandom(colors)) 
 
-  const getNewValues=()=>{
-    const newQuote=getRandom(quoteDB)
-    const newColor=getRandom(colors)
+  const [quote, setQuote] = useState(getRandom(quoteDB))
+  const [color, setColor] = useState(getRandom(colors))
+
+  const getNewValues = () => {
+    const newQuote = getRandom(quoteDB)
+    const newColor = getRandom(colors)
     setQuote(newQuote)
     setColor(newColor)
   }
 
-  const backgroundObject={
+  const backgroundObject = {
     backgroundColor: color
+  }
+
+  const colorObject = {
+    color: color
   }
 
   return (
     <div className="App" style={backgroundObject}>
       <QuoteBox 
-      quote={quote} 
-      getNewValues={getNewValues}
-      backgroundObject={backgroundObject} 
-    />
+        quote={quote} 
+        getNewValues={getNewValues} 
+        backgroundObject={backgroundObject}
+        colorObject={colorObject}
+      />
     </div>
   )
 }
--- src/components/QuoteButton.jsx
+++ src/components/QuoteButton.jsx
@@ -1,11 +1,9 @@
-const QuoteButton = ({getNewValues,backgroundObject}) => {
-
+const QuoteButton = ({getNewValues, backgroundObject}) => {
   
   return (
-  <button onClick={getNewValues} style={backgroundObject}>
-    <i className='bx bx-chevron-right'></i>    
-  </button>
+    <button onClick={getNewValues} style={backgroundObject}>
+      <i className='bx bx-chevron-right'></i>
+    </button>
   )
 }
-
 export default QuoteButton
